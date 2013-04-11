991158aacdd54d1da83c422580e8cc32b71db346
Ky-Anh Huynh <kyanh@theslinux.org>
2013-04-11 22:09:47 +0000

    scim2umi now supports VNI and options
    . --telex: used to convert Telex table
    . --vni: used to convert VNI table

--- bin/scim2uim.rb
+++ bin/scim2uim.rb
@@ -4,17 +4,33 @@
 # Purpose: Convert the Scim table to Uim table
 # Author : Anh K. Huynh
 # Date   : 2013 Mar 7th
-# FIXME  : This only works with the Telex table
+# Usage  :
+#   $0 --telex < src/Telex.txt.in > xteltex.scm
+#   $0 --vni   < src/VNI.txt.in   > xvni.scm
+
+mode = true
+ARGV.each do |p|
+  mode = case p
+  when "--telex" then true
+  when "--vni"   then false
+  end
+end
+
+name = {true => "Telex", false => "VNI"} [mode]
+xname = {true => "xtelex", false => "xvni"} [mode]
+
+reg  = {true => /^(\p{Alpha}+)\p{Space}+([^\p{Space}]+)\p{Space}+0$/,
+        false => /^(\p{Alnum}+)\p{Space}+([^\p{Space}]+)\p{Space}+0$/ } [mode]
 
 puts <<-EOF
 (require "generic.scm")
-(define xtelex-rule
+(define #{xname}-rule
   '(
 EOF
 
 STDIN.readlines.each do |line|
   line.strip!
-  if gs = line.match(/^(\p{Alpha}+)\p{Space}+([^\p{Space}]+)\p{Space}+0$/)
+  if gs = line.match(reg)
     input, output = gs[1,2]
     input = input.split(//).map{|c| "\"#{c}\""}.join(" ")
     puts sprintf("(((%s ))(\"%s\"))", input, output)
@@ -23,15 +39,15 @@ end
 
 puts <<-EOF
   ))
-(define xtelex-init-handler
+(define #{xname}-init-handler
   (lambda (id im arg)
-    (generic-context-new id im xtelex-rule #f)))
+    (generic-context-new id im #{xname}-rule #f)))
 
 (generic-register-im
- 'xtelex
+ '#{xname}
  "vi"
  "UTF-8"
- (N_ "XTelex")
- (N_ "Big table of predefined words for Vietnamese Telex users")
- xtelex-init-handler)
+ (N_ "X#{name}")
+ (N_ "Big table of predefined words for Vietnamese #{name} users")
+ #{xname}-init-handler)
 EOF
