bea886a66e9e93e7602fc5b45af240d3df14b801
Ky-Anh Huynh <kyanh@theslinux.org>
2013-04-17 20:45:27 +0000

    Add link to license in generated file

--- bin/scim2uim.rb
+++ bin/scim2uim.rb
@@ -28,11 +28,13 @@ puts <<-EOF
 ;;
 ;; For more details please visit the project home page
 ;;   https://github.com/icy/myquartz-scim2uim
+;;   https://github.com/TheSLinux-forks/myquartz-scim2uim
 ;;
 ;; This work is distributed under the license GPL (v2).
 ;; You must read and accept the license if you want to
-;; use, distribute, modify this work, and/or to
-;; create any derivative work.
+;; use, distribute, modify this work, and/or to create
+;; any derivative work. The license can be found at
+;;   http://www.gnu.org/licenses/gpl-2.0.html
 ;;
 (require "generic.scm")
 (define #{xname}-rule
