b4c552b0de5811912386d911bbe878504c239f22
Ky-Anh Huynh <kyanh@theslinux.org>
2013-04-17 20:51:15 +0000

    + script "sort.rb to sort the original Scim table

---
+++ bin/sort.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+# encoding=utf-8
+
+# Purpose: Sort the original tables on the branch `scim`
+#          and prepare new tables for UIM.
+# Author : Anh K. Huynh
+# Date   : 2013 Mar 17th
+# Usage  : This script should be used **ONCE**
+#
+#   $0 --telex < src/Telex.txt.in > Telex.txt.in.new
+#   $0 --vni   < src/VNI.txt.in   > VNI.txt.in.new
+#   $mv Telex.txt.in.new Telex.txt.in
+#   $mv VNI.txt.in.new   VNI.txt.in
+
+output = STDIN \
+  .readlines \
+  .select do |line|
+    line.match(%r{^(\p{Alnum}+)\p{Space}+([^\p{Space}]+)\p{Space}+0$})
+  end \
+  .map do |line|
+     line.split.slice(0,2)
+  end \
+  .sort do |foo, bar|
+    ai, bi = foo[0], bar[0]
+    ai.size == bi.size \
+      ? ai <=> bi \
+      : ai.size <=> bi.size
+  end
+
+puts <<-EOF
+;;
+;; Table of predefined combinations for Vietnamese Telex/VNI users.
+;;
+;; This table is generated from the original table from the branch `scim`.
+;; Once this table is manually edit, the generator must be disabled and
+;; any further udpate must be done manually.
+;;
+;; This work is distributed under the terms of the license GPL (v2).
+;; To use this work you must read and accept the license that's found at
+;;   http://www.gnu.org/licenses/gpl-2.0.html
+;;
+EOF
+
+puts output.map{|u,v| "#{u}\t#{v}"}.join("\n")
